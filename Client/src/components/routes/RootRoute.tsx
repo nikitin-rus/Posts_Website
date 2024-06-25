08260b44529231a35beddeb53ffd18a71dea70ef
Nikitin Ruslan <mystudychannel.0@gmail.com>
2024-06-25 15:12:08 +0300

    save login data to localStorage

--- Client/src/components/routes/RootRoute.tsx
+++ Client/src/components/routes/RootRoute.tsx
@@ -1,7 +1,33 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { authSelector, login } from "../../redux/slices/authSlice";
+import { useEffect } from "react";
+import { getUserFromLocalStorage } from "../../helpers/getUserFromLocalStorage";
 
 export function RootRoute() {
+    const auth = useSelector(authSelector);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const user = getUserFromLocalStorage();
+        const jwt = localStorage.getItem('jwt');
+
+        if (user && jwt) {
+            dispatch(login({ user: user, jwtToken: jwt }));
+        }
+    }, []);
+
+    useEffect(() => {
+        if (auth.jwtToken && auth.user) {
+            localStorage.setItem('jwt', auth.jwtToken);
+            localStorage.setItem('user', JSON.stringify(auth.user));
+        } else {
+            localStorage.removeItem('jwt');
+            localStorage.removeItem('user');
+        }
+    }, [auth]);
+
     return (
         <div className="root-route">
             <Navbar />
