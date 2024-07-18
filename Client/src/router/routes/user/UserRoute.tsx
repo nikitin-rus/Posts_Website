84cb3ba1a4d7d9b4eb721b19f1f4c4ac8338fd6f
Nikitin Ruslan <mystudychannel.0@gmail.com>
2024-07-18 12:02:46 +0300

    decompose UserRoute; add UserCard

--- Client/src/router/routes/user/UserRoute.tsx
+++ Client/src/router/routes/user/UserRoute.tsx
@@ -2,39 +2,26 @@ import { Outlet, useLoaderData } from "react-router-dom";
 import { Page } from "../../../components/Page";
 import { Tabs } from "../../../components/Tabs";
 import { UserSchema } from "../../../schemas/user/UserSchema";
-import { authSelector } from "../../../redux/slices/authSlice";
-import { useAppSelector } from "../../../redux/hooks";
+import { UserCard } from "../../../components/cards/UserCard";
 
 export function UserRoute() {
     const componentClassName = "user-route";
-
-    const auth = useAppSelector(authSelector);
 
     const user = UserSchema.parse(useLoaderData());
 
     return (
         <div className={componentClassName}>
             <Page>
-                <section className={componentClassName + "__section"}>
-                    <div className={componentClassName + "__info"}>
-                        <h1 className={componentClassName + "__heading"}>
-                            @{user.userName}
-                        </h1>
-
-                        {auth.user?.userName === user.userName && (
-                            <p className={componentClassName + "__text"}>
-                                Email: {user.email}
-                            </p>
-                        )}
-                    </div>
-
+                <UserCard className={componentClassName + "__user-card"}
+                    user={user}
+                >
                     <Tabs className={componentClassName + "__tabs"}
                         tabs={[
                             { title: "Посты", link: `/users/${user.id}/posts` },
                             { title: "Комментарии", link: `/users/${user.id}/comments` },
                         ]}
                     />
-                </section>
+                </UserCard>
 
                 <Outlet />
             </Page>
